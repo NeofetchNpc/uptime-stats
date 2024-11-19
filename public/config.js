b768fc960956f4c4c151b62c591a39ad6a1aafce
Npc <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-19 11:20:05 +0700

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,10 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1750065-b4f1a16b3a7a2fb0d2505154',
   ],
 
   // 日志天数
@@ -22,15 +19,15 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: 'https://uptime.neastooid.xyz'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/NeofetchNpc/uptime-stats'
     },
     {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: 'Rest Api',
+      url: 'https://api.neastooid.xyz'
     },
   ],
 };
